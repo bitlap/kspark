85ae6dc0766bf6f315e64a1540f038c3f20e4392
IceMimosa <chk19940609@gmail.com>
2019-01-07 19:38:28 +0800

    Init project

---
+++ core/build.gradle.kts
@@ -0,0 +1,5 @@
+dependencies {
+    compile("org.apache.spark", "spark-core_2.12", "2.4.0")
+    compile("org.apache.spark", "spark-sql_2.12", "2.4.0")
+    compile("org.apache.spark", "spark-hive_2.12", "2.4.0")
+}
