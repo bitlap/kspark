ef29ba591e26a69d4ffee1c56d51f862bf3cf92b
IceMimosa <chk19940609@gmail.com>
2019-01-12 14:29:22 +0800

    Add functions

--- core/src/main/kotlin/io/patamon/spark/kt/KSpark.kt
+++ core/src/main/kotlin/io/patamon/spark/kt/KSpark.kt
@@ -1,6 +1,7 @@
 package io.patamon.spark.kt
 
 import org.apache.spark.sql.SparkSession
+import java.io.Serializable
 import kotlin.reflect.KClass
 
 /**
@@ -14,9 +15,34 @@ fun spark(context: KSparkContext.() -> Unit): KSpark =
     KSparkContext().apply(context).getOrCreate()
 
 
-class KSpark(private val spark: SparkSession) {
+open class KSpark(val spark: SparkSession) : Serializable {
+
+    val sc = spark.sparkContext()
+    val sparkContext = sc
+    val sqlContext = spark.sqlContext()
+    val conf by lazy { spark.conf() }
+    val catalog by lazy { spark.catalog() }
+
+    fun version() = spark.version()
+    fun read() = spark.read()
+    fun readStream() = spark.readStream()
+    fun table(tableName: String): DataFrame = spark.table(tableName)
+    // fun udf() = spark.udf()
 
     fun createDataFrame(data: List<*>, beanClass: KClass<*>): DataFrame = spark.createDataFrame(data, beanClass.java)
+    fun emptyDataFrame(): DataFrame = spark.emptyDataFrame()
+    fun sql(sql: String): DataFrame = spark.sql(sql)
 
+
+    fun cloneSession() = KSpark(spark.cloneSession())
+    fun newSession() = KSpark(spark.newSession())
+
+
+    fun stop() = spark.stop()
+    fun close() = spark.close()
+
+    fun test() {
+
+    }
 }
 
