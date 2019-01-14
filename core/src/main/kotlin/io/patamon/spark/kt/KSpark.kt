fa0bcba929626c8a9db474259e7c4576b60bd389
IceMimosa <chk19940609@gmail.com>
2019-01-14 21:13:24 +0800

    Add functions

--- core/src/main/kotlin/io/patamon/spark/kt/KSpark.kt
+++ core/src/main/kotlin/io/patamon/spark/kt/KSpark.kt
@@ -1,5 +1,8 @@
 package io.patamon.spark.kt
 
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.mapred.InputFormat
+import org.apache.hadoop.mapred.JobConf
 import org.apache.spark.api.java.JavaRDD
 import org.apache.spark.api.java.JavaSparkContext
 import org.apache.spark.rdd.RDD
@@ -8,6 +11,7 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.StructType
 import java.io.Serializable
 import kotlin.reflect.KClass
+import org.apache.hadoop.mapreduce.InputFormat as NewInputFormat
 
 /**
  * Desc:
@@ -22,6 +26,7 @@ fun spark(context: KSparkContext.() -> Unit): KSpark =
 
 open class KSpark(val spark: SparkSession) : Serializable {
 
+    // Inside Fields
     val sc = spark.sparkContext()
     val jsc = JavaSparkContext(sc)
     val sparkContext = sc
@@ -30,12 +35,18 @@ open class KSpark(val spark: SparkSession) : Serializable {
     val conf by lazy { spark.conf() }
     val catalog by lazy { spark.catalog() }
 
+    // Inside Functions
     fun version() = spark.version()
     fun read() = spark.read()
     fun readStream() = spark.readStream()
     fun table(tableName: String): DataFrame = spark.table(tableName)
-    // fun udf() = spark.udf()
+    fun udf() = spark.udf() // TODO
+    fun cloneSession() = KSpark(spark.cloneSession())
+    fun newSession() = KSpark(spark.newSession())
+    fun stop() = spark.stop()
+    fun close() = spark.close()
 
+    // Inside Functions Create DataFrame
     fun createDataFrame(data: List<*>, beanClass: KClass<*>): DataFrame = spark.createDataFrame(data, beanClass.java)
     fun createDataFrame(data: List<Row>, structType: StructType): DataFrame = spark.createDataFrame(data, structType)
     fun createDataFrame(data: RDD<*>, beanClass: KClass<*>): DataFrame = spark.createDataFrame(data, beanClass.java)
@@ -46,11 +57,46 @@ open class KSpark(val spark: SparkSession) : Serializable {
     fun emptyDataFrame(): DataFrame = spark.emptyDataFrame()
     fun sql(sql: String): DataFrame = spark.sql(sql)
 
-    fun cloneSession() = KSpark(spark.cloneSession())
-    fun newSession() = KSpark(spark.newSession())
+    // Extend Functions
+    fun <T> parallelize(data: List<T>, numSlices: Int = jsc.defaultParallelism()) = jsc.parallelize(data, numSlices)
+    fun hadoopConfiguration() = jsc.hadoopConfiguration()
+    fun <K, V, F : InputFormat<K, V>> hadoopRDD(
+        conf: JobConf,
+        inputFormatClass: Class<out F>,
+        keyClass: Class<K>,
+        valueClass: Class<V>,
+        minPartitions: Int = jsc.defaultMinPartitions()
+    ) = jsc.hadoopRDD(conf, inputFormatClass, keyClass, valueClass, minPartitions)
+    fun <K, V, F : InputFormat<K, V>> hadoopFile(
+        path: String,
+        inputFormatClass: Class<out F>,
+        keyClass: Class<K>,
+        valueClass: Class<V>,
+        minPartitions: Int = jsc.defaultMinPartitions()
+    ) = jsc.hadoopFile(path, inputFormatClass, keyClass, valueClass, minPartitions)
+    fun <T> broadcast(value: T) = jsc.broadcast(value)
+    fun <K, V, F : NewInputFormat<K, V>> newAPIHadoopRDD(
+        conf: Configuration,
+        fClass: Class<out F>,
+        kClass: Class<K>,
+        vClass: Class<V>
+    ) = jsc.newAPIHadoopRDD(conf, fClass, kClass, vClass)
+    fun<K, V, F : NewInputFormat<K, V>> newAPIHadoopFile(
+        path: String,
+        fClass: Class<out F>,
+        kClass: Class<K>,
+        vClass: Class<V>,
+        conf: Configuration
+    ) = jsc.newAPIHadoopFile(path, fClass, kClass, vClass, conf)
+    fun <K, V> sequenceFile(
+        path: String,
+        keyClass: Class<K>,
+        valueClass: Class<V>,
+        minPartitions: Int = jsc.defaultMinPartitions()
+    ) = jsc.sequenceFile(path, keyClass, valueClass, minPartitions)
+    fun <T> objectFile(path: String, minPartitions: Int = jsc.defaultMinPartitions()) = jsc.objectFile<T>(path, minPartitions)
+    fun textFile(path: String, minPartitions: Int = jsc.defaultMinPartitions()) = jsc.textFile(path, minPartitions)
+    fun wholeTextFiles(path: String, minPartitions: Int = jsc.defaultMinPartitions()) = jsc.wholeTextFiles(path, minPartitions)
 
-
-    fun stop() = spark.stop()
-    fun close() = spark.close()
 }
 
