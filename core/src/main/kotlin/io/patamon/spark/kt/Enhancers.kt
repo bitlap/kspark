71c87b8baa74b437d19b494d64fead78b0bd8023
IceMimosa <chk19940609@gmail.com>
2019-02-25 12:03:59 +0800

    Add save functions

--- core/src/main/kotlin/io/patamon/spark/kt/Enhancers.kt
+++ core/src/main/kotlin/io/patamon/spark/kt/Enhancers.kt
@@ -1,6 +1,45 @@
 package io.patamon.spark.kt
 
+import org.apache.spark.sql.SaveMode
+
 /**
  * List to DataFrame
  */
 inline fun <reified T: Any> List<T>?.toDF(spark: KSpark): DataFrame = spark.createDataFrame(this ?: emptyList())
+
+/**
+ * Save functions
+ */
+fun DataFrame.save(
+    path: String,
+    format: String = "parquet",
+    mode: SaveMode = SaveMode.ErrorIfExists,
+    partitionBy: List<String> = emptyList(),
+    options: Map<String, String> = emptyMap()
+) {
+    if (!this.isEmpty) {
+        this.write().format(format)
+            .mode(mode)
+            .partitionBy(*partitionBy.toTypedArray())
+            .options(options)
+            .save(path)
+    }
+}
+
+fun DataFrame.saveAsParquet(
+    path: String,
+    mode: SaveMode = SaveMode.ErrorIfExists,
+    partitionBy: List<String> = emptyList(),
+    options: Map<String, String> = emptyMap()
+) {
+    this.save(path, "parquet", mode, partitionBy, options)
+}
+
+fun DataFrame.saveAsOrc(
+    path: String,
+    mode: SaveMode = SaveMode.ErrorIfExists,
+    partitionBy: List<String> = emptyList(),
+    options: Map<String, String> = emptyMap()
+) {
+    this.save(path, "orc", mode, partitionBy, options)
+}
