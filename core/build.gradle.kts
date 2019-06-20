c7b7787602e43e50d027ef0d69732f44ef744918
IceMimosa <chk19940609@gmail.com>
2019-06-20 23:59:43 +0800

    Upgrade kotlin version

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,5 +3,5 @@ dependencies {
     compile("org.apache.spark", "spark-core_2.12", sparkVersion)
     compile("org.apache.spark", "spark-sql_2.12", sparkVersion)
     compile("org.apache.spark", "spark-hive_2.12", sparkVersion)
-    compile("org.jetbrains.kotlin", "kotlin-reflect", "1.3.31")
+    compile("org.jetbrains.kotlin", "kotlin-reflect", "1.3.40")
 }
